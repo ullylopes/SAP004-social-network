3c4db37e0719e23b8cbcba118aa372030347a755
sabia997 <sabi.sm12@gmail.com>
2020-06-01 18:58:23 -0300

    Ajeitando SAP

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 // Este é o ponto de entrada de sua aplicação
-import cadastro from './pages/cadastro.js';
-import login from './pages/login.js';
+/* import cadastro from './pages/cadastro.js';
+import login from './pages/login.js'; */
+import routs from "./routs.js"
 
 const main = document.querySelector('#root');
 
@@ -9,18 +10,18 @@ const init =() => {
         main.innerHTML=" "
         switch(window.location.hash){
             case " ":
-              main.appendChild(login());
+              main.appendChild(routs["login"]);
               break;
             case "#cadastro":
-                main.appendChild(cadastro());
+                main.appendChild(routs["cadastro"]);
                 break;
             default:
-                main.appendChild(login())
+                main.appendChild(routs["login"]);
         }
     })
 }
 window.addEventListener("load", () => {
-    main.appendChild(login());
+    main.appendChild(routs["login"]);
     init();
 
 } )
