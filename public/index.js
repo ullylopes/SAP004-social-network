f852f78b4ea608163adc10775a7a3f6d8737673c
Adelia <adeliacristinecs@gmail.com>
2020-06-01 15:42:02 -0300

    teste iniciais

--- public/index.js
+++ public/index.js
@@ -1,4 +1,18 @@
 // Este é o ponto de entrada de sua aplicação
-import { home } from './pages/home/main.js';
+import   './pages/home/main.js'; 
 
 /* document.querySelector('#root').appendChild(home()); */
+/* window.addEventListener("load,") */
+document.getElementById("cadastro").addEventListener("click",()=>{
+  console.log("clicou")
+})
+
+firebase.auth().signInWithEmailAndPassword(email, password)
+.then(()=>HTMLFormControlsCollection.log("deu certo"))
+.catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log("deu ruim")// ...
+});
+email-password.html;
---
+++ public/pages/home/index.js
@@ -0,0 +1,5 @@
+export default() => {
+    const container = document.createElement("div");
+const cadastro =`
+`
+}
